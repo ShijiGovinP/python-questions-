327fb0a51252a0cedde1e5119e75632ba0ae39c5
ShijiGovinP <shijigovin2004@gmail.com>
2025-01-26 20:50:42 +0530

    question 2.py
    Using if,elseif concepts ,checking the given numbers whether >=, <, <=, ==, and != and print the statements according to the conditions.

---
+++ question 2.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+if(a>b):
+    print(" a is greater:")
+elif(a>=b):
+    print("a is greater than or equal to b:")
+elif(a<=b):
+    print("a is less than or eaual to b:")
+elif(a<b):
+    print("a is less than b:")
+elif(a==b):
+    print("a is equals to b:")
+elif(a!=b):
+    print("a is not equal to b:")
