fe71e0ba7f9e05f713c73ab42ab26c5ef576f4eb
ShijiGovinP <shijigovin2004@gmail.com>
2025-01-26 21:17:48 +0530

    program.py
    using conditional statements checking which number is belonging to their number.

---
+++ program.py
@@ -0,0 +1,7 @@
+a=int(input("enter the belongings"))
+if(a>50):
+    print("the number belonging to 50:")
+elif(a<50 or a<40):
+    print("the number belonging between 40 and 50:")
+else:
+    print("the number belonging to less than 50")
