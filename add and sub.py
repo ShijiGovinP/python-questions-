906a69e5db28aae2406cdd94e98f5133437c03e9
ShijiGovinP <shijigovin2004@gmail.com>
2025-01-28 21:46:37 +0530

    add and sub.py

---
+++ add and sub.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+if subtraction < 0:
+    subtraction = -subtraction  
+print("Addition result:", addition)
+print("Subtraction result (positive):", subtraction)
+    
