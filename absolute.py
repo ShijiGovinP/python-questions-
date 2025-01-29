233b2cbfa57fae7b970e58ff59af864a58cfa4d2
ShijiGovinP <shijigovin2004@gmail.com>
2025-01-29 19:28:24 +0530

    absolute.py

---
+++ absolute.py
@@ -0,0 +1,6 @@
+n=int(input("enter the value:"))
+s=int(input("enter the value:"))
+result=n+s # adding the two values
+result1=abs(n-s) # absolute(abs) function  used for changing negative value into positive value,here subtracting the valuse
+print("the result is:",result)
+print("The abolute of value:",result1)
