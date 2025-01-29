d2aa30dad0150f04bc11f7032c8b8b64d8d22e32
ShijiGovinP <shijigovin2004@gmail.com>
2025-01-29 19:20:39 +0530

    multiple.py

---
+++ multiple.py
@@ -0,0 +1,7 @@
+def num(n):
+    if (n%27==0): # checks the value is dividing by 27 of getting the reaminder as 0 by %
+        print(" multiple of the value:")
+    else:
+         print("not a multiple of value:")
+n=int(input("Enter the value:"))
+num(n) #object calling
