a478cf73acea4d063fe34a9b53957dd910800071
ShijiGovinP <shijigovin2004@gmail.com>
2025-02-06 06:38:25 +0530

    SIMPLEINTEREST.py

---
+++ SIMPLEINTEREST.py
@@ -0,0 +1,6 @@
+P=int(input("ENTER THE PRINCIPAL:"))
+T=int(input("ENTER THE TIME:"))
+R=int(input("ENTER THE RATE OF INTEREST:"))
+
+simpleinterest = (P*T*R)/100 //calculating all the values
+print(simpleinterest)
